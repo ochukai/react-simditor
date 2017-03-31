c5b6dfd6b4a6b82e5d245a3e27f560b4440e87ef
ochukai <sex.xiaoyee@gmail.com>
2017-03-31 13:55:17 +0800

    finish

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # react-simditor
+
+```sh
+npm install simple-module --save
+npm install simple-uploader --save
+npm install simditor --save
+```
+
+> 不要忘了修改一下 upload 的 url
---
+++ SimEditor.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'simple-module';
+import 'simple-uploader';
+import Simditor from 'simditor';
+
+import 'simditor/styles/simditor.css';
+
+import CSSModule from 'react-css-modules';
+import styles from './SimEditor.less';
+
+class SimEditor extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentDidMount() {
+    const textbox = ReactDOM.findDOMNode(this.refs.textarea);
+
+    const $textbox = $(textbox);
+    const editor = this.editor = new Simditor({
+      textarea: $textbox,
+      toolbar: [
+        'title',
+        'bold',
+        'italic',
+        'underline',
+        'strikethrough',
+        'fontScale',
+        'color',
+        'ol',             // ordered list
+        'ul',             // unordered list
+        'blockquote',
+        'code',           // code block
+        'table',
+        'link',
+        'image',
+        'hr',             // horizontal ruler
+        'indent',
+        'outdent',
+        'alignment',
+      ],
+      placeholder: '',
+      defaultImage: '',
+      pasteImage: true,
+      imageButton: 'upload',
+      upload: {
+        url: '/file/uploadImageForSimEditor?fileElementId=upload_file',
+        fileKey: 'upload_file',
+        connectionCount: 3,
+        leaveConfirm: '还在上传图片，确定要离开吗?',
+      },
+    });
+
+    editor.setValue(this.props.content);
+
+    // 修复上传打开慢的问题
+    $('.toolbar-item-image input[type="file"]')
+      .removeAttr('multiple')
+      .attr('accept', 'image/gif, image/jpeg, image/jpg, image/png');
+  }
+
+  render() {
+    return (
+      <div>
+        <textarea ref="textarea" />
+      </div>
+    );
+  }
+}
+
+SimEditor.defaultProps = {
+  content: '',
+};
+
+export default SimEditor;
